3481b5490bff3e1876fcef530632ec4b86d62093
seosang-won <zmdk1205@naver.com>
2024-09-23 15:07:31 +0900

    init

---
+++ src/main/java/com/example/devcoursed/DevcoursedApplication.java
@@ -0,0 +1,13 @@
+package com.example.devcoursed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevcoursedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevcoursedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/devcoursed/DevcoursedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devcoursed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevcoursedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
