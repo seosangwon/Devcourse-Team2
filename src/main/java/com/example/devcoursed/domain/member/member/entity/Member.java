8e55320e7806437d2448e9d35d4b86ef6893641f
seosang-won <zmdk1205@naver.com>
2024-09-24 14:37:54 +0900

    feat : TT-15 set basic orders,orderItem,product entity

--- src/main/java/com/example/devcoursed/domain/member/member/entity/Member.java
+++ src/main/java/com/example/devcoursed/domain/member/member/entity/Member.java
@@ -1,23 +1,27 @@
 package com.example.devcoursed.domain.member.member.entity;
 
+import com.example.devcoursed.domain.orders.orders.entity.Orders;
+import com.example.devcoursed.domain.prodcut.prodcut.entity.Product;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.query.Order;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
 
 import static lombok.AccessLevel.PROTECTED;
 
 @Entity
-@Setter
 @Getter
 @AllArgsConstructor(access = PROTECTED)
 @NoArgsConstructor(access = PROTECTED)
 @EntityListeners(AuditingEntityListener.class)
-@ToString
 @EqualsAndHashCode
+@Builder
 public class Member {
 
     @Id
@@ -26,7 +30,7 @@ public class Member {
     private Long id;
 
     @Column(unique = true)
-    private String mId;
+    private String loginId;
 
     private String pw;
     private String name;
@@ -36,6 +40,20 @@ public class Member {
 
     @CreatedDate
     private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
+
+    @OneToMany(mappedBy = "maker" , cascade = CascadeType.ALL , orphanRemoval = true)
+    @ToString.Exclude
+    List<Product> productList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "member" , cascade = CascadeType.ALL , orphanRemoval = true)
+    @ToString.Exclude
+    List<Orders> ordersList = new ArrayList<>();
+
+
+
 
 
 
