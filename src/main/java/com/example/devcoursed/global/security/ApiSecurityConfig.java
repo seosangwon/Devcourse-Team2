31eb92507f3832a4f95ad4593d24dff69c515d75
seosang-won <zmdk1205@naver.com>
2024-09-25 11:14:29 +0900

    feat : TT-16 add spring security dependencies

---
+++ src/main/java/com/example/devcoursed/global/security/ApiSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.example.devcoursed.global.security;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApiSecurityConfig {
+
+
+
+
+
+
+
+
+}
