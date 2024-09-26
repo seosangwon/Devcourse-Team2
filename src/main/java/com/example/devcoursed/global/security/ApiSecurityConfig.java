e6e11cf1f683cfdbd10dcf6b7a8e8d2375d6ed1f
seosangwon <39723498+seosangwon@users.noreply.github.com>
2024-09-26 16:39:26 +0900

    [Feat] JWT 필터 체인을 구현하다 (#9)
    * feat: [BE-Feat] Add JWT dependency
    
    * feat: [BE-Feat] update JWT dependency
    
    * feat: [BE-Feat] update JWT dependency 0.12.3
    
    * feat: TT-19 Add JwtUtil
    
    * feat: TT-19 Add JwtUtilTest
    
    * feat: TT-19 Add authenticate logic in JwtFilterChain and Add SecurityUser
    
    * feat: TT-19 비회원은 로그인,회원가입 , 나머지 api 요청사항은 인증 필요 구현 추가
    
    * refactor: TT-19 인코딩시 UTF-8 추가

--- src/main/java/com/example/devcoursed/global/security/ApiSecurityConfig.java
+++ src/main/java/com/example/devcoursed/global/security/ApiSecurityConfig.java
@@ -2,8 +2,10 @@ package com.example.devcoursed.global.security;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -11,23 +13,29 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@EnableWebSecurity
+@EnableMethodSecurity
 @RequiredArgsConstructor
+@Slf4j
 public class ApiSecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
     @Bean
     SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
         http
                 .securityMatcher("/api/**")
                 .csrf(AbstractHttpConfigurer::disable)
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-        ;
+                .authorizeHttpRequests(authorize -> authorize
+                        // 인증 없이 접근 가능한 엔드포인트
+                        .requestMatchers("/api/v1/members/login", "/api/v1/members/register").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        log.info("JWT 필터 통과 ");
+
         return http.build();
     }
-
-
-
 
 
 }
