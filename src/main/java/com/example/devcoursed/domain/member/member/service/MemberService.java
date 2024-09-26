8a8fa3ef7fc83f4c5b7a0c476d82926f72e29561
asklas <jyjy0372@naver.com>
2024-09-26 10:30:16 +0900

    + DTO Inner Class로 관리 + Service CRUD 기본구성 + RepositoryTest 일괄실행 가능

---
+++ src/main/java/com/example/devcoursed/domain/member/member/service/MemberService.java
@@ -0,0 +1,67 @@
+package com.example.devcoursed.domain.member.member.service;
+
+import com.example.devcoursed.domain.member.member.dto.MemberDTO;
+import com.example.devcoursed.domain.member.member.entity.Member;
+import com.example.devcoursed.domain.member.member.repository.MemberRepository;
+
+import java.util.Map;
+import java.util.Optional;
+
+public class MemberService {
+    private MemberRepository memberRepository;
+
+    public MemberDTO.Create create(MemberDTO.Create dto) {
+        memberRepository.save(dto.toEntity());
+        return dto;
+    }
+    public MemberDTO.Update update(MemberDTO.Update dto) {
+        Optional<Member> memberOptional = memberRepository.findById(dto.getId());
+
+        if (memberOptional.isPresent()) {
+            Member member = memberOptional.get();
+            member.changeLoginId(dto.getLoginId());
+            member.changePw(dto.getPw());
+            member.changeName(dto.getName());
+            member.changeMImage(dto.getMImage());
+            memberRepository.save(member);
+
+            return new MemberDTO.Update(
+                    member.getId(),
+                    member.getLoginId(),
+                    member.getPw(),
+                    member.getName(),
+                    member.getMImage()
+            );
+        } else {
+            return null; //Exception 처리 예정
+        }
+    }
+
+    public void delete(Long id) {
+        Optional<Member> memberOptional = memberRepository.findById(id);
+        if (memberOptional.isPresent()) {
+            Member member = memberOptional.get();
+            memberRepository.delete(member);
+        } else {
+            //Exception 처리
+        }
+    }
+
+    public MemberDTO.Response read(Long id) {
+        Optional<Member> memberOptional = memberRepository.findById(id);
+        if (memberOptional.isPresent()) {
+            Member member = memberOptional.get();
+            return new MemberDTO.Response(
+                    member.getLoginId(),
+                    member.getPw(),
+                    member.getName(),
+                    member.getMImage(),
+                    member.getCreatedAt(),
+                    member.getModifiedAt()
+            );
+        } else {
+            return null; //Exception 처리
+        }
+    }
+
+}
