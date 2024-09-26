6fbfff32c6e9a4aae4525d8251687ecfc78525cb
seosang-won <zmdk1205@naver.com>
2024-09-26 10:02:37 +0900

    feat: TT-19 Add JwtUtil

--- src/main/java/com/example/devcoursed/global/util/JwtUtil.java
+++ src/main/java/com/example/devcoursed/global/util/JwtUtil.java
@@ -3,7 +3,10 @@ package com.example.devcoursed.global.util;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 
+import javax.crypto.SecretKey;
 import java.util.Date;
 import java.util.Map;
 
@@ -11,21 +14,40 @@ public class JwtUtil {
 
     private static final String SECRET_KEY = "abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890";
 
-    public static String encode(long expirationSeconds , Map<String , Object> data) {
-        Claims claims = Jwts.claims()
+    private static final SecretKey key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
 
+
+    public static String encode(Map<String, Object> data) {
+
+        Claims claims = Jwts
+                .claims()
+                .add("data", data)
+                .add("type", "access_token")
                 .build();
+
         Date now = new Date();
-        Date expiration = new Date(now.getTime() + 1000 * expirationSeconds);
+        Date expiration = new Date(now.getTime() + 1000 * 60 * 5); // 1초 X 60 X 5
 
         return Jwts.builder()
+                .subject("NBE2_T2")
                 .claims(claims)
                 .issuedAt(now)
                 .expiration(expiration)
-                .signWith(SignatureAlgorithm.HS256 , SECRET_KEY)
+                .signWith(key)
                 .compact();
 
 
     }
+
+    public static Claims decode(String token) {
+        return Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+
+
+    }
+
 
 }
