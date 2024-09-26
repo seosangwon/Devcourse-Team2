c1a2c3831bc6c47bdfc7366e9c7343b44bc5b653
seosang-won <zmdk1205@naver.com>
2024-09-26 17:10:01 +0900

    refactor : TT-20 move webConfig to global

--- src/main/java/com/example/devcoursed/global/config/WebConfig.java
+++ src/main/java/com/example/devcoursed/global/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.example.devcoursed.domain.config;
+package com.example.devcoursed.global.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
