92237c27a34a1ebf5049734075d965486ccbb027
1mjay <sumthq@gmail.com>
2024-09-27 11:43:08 +0900

    feat : TT-23 Product insert, update

--- src/main/java/com/example/devcoursed/domain/product/product/repository/ProductRepository.java
+++ src/main/java/com/example/devcoursed/domain/product/product/repository/ProductRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    Optional<Product> findByMaker(Member maker);
+    Optional<Product> findByMakerAndName(Member maker, String name);
 
 }
