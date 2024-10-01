8f92e162f13a76c785593464da34cc95546246c0
asklas <jyjy0372@naver.com>
2024-10-01 16:11:53 +0900

    indexPage 구성

--- front/src/components/Login.js
+++ front/src/components/Login.js
@@ -18,7 +18,7 @@ function Login({ onLogin }) {
 
             localStorage.setItem('token', accessToken); // JWT 저장
             localStorage.setItem('id', id); // JWT 저장
-            onLogin(name, mimage); // 로그인 후 사용자 이름과 이미지 전달
+            onLogin( name, mimage ); // 로그인 후 사용자 이름과 이미지 전달
         } catch (error) {
             setErrorMessage('로그인 실패: ' + (error.response?.data?.message || '서버에 문제가 발생했습니다.'));
         }
