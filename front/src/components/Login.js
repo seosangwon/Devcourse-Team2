b1d75c500c99cf7ff849b33afc1fc27057054241
asklas <jyjy0372@naver.com>
2024-10-01 14:05:53 +0900

    Conflict 문제 해결

--- front/src/components/Login.js
+++ front/src/components/Login.js
@@ -14,9 +14,10 @@ function Login({ onLogin }) {
 
         try {
             const response = await axios.post('/api/v1/members/login', dataToSend);
-            const { accessToken, name, mimage } = response.data; // mImage 추가
+            const { id, accessToken, name, mimage } = response.data; // mImage 추가
 
             localStorage.setItem('token', accessToken); // JWT 저장
+            localStorage.setItem('id', id); // JWT 저장
             onLogin(name, mimage); // 로그인 후 사용자 이름과 이미지 전달
         } catch (error) {
             setErrorMessage('로그인 실패: ' + (error.response?.data?.message || '서버에 문제가 발생했습니다.'));
