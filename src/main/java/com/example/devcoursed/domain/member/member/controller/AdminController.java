20e7df72d248de4663a0a8237555a479fbe8d5c0
wnsgho <darkblueuu@gmail.com>
2024-10-08 14:04:58 +0900

    발주 등록 시 평균단가 버튼 수정

--- src/main/java/com/example/devcoursed/domain/member/member/controller/AdminController.java
+++ src/main/java/com/example/devcoursed/domain/member/member/controller/AdminController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1")
+@RequestMapping("/api/adm")
 public class AdminController {
     private final MemberService memberService;
     private final ProductService productService;
